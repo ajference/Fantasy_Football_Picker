394f4856920d47226647771ce55945df7cf2d2fd
Alejandro Quintana <aquintana96@email.arizona.edu>
2018-11-03 16:46:51 -0700

    test 1

--- src/nfl/Player.java
+++ src/nfl/Player.java
@@ -1,5 +1,31 @@
 package nfl;
 
 public class Player {
-
+	String name;
+	String team;
+	int rank;
+	
+	
+	public Player() {
+		this.name = "";
+		this.team = "";
+		this.rank = 0; 
+	}
+	
+	public void setName(String i) {
+		this.name = i;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setRank(int i) {
+		this.rank = i;
+	}
+	public int getRank() {
+		return this.rank;
+	}
+	
+	
 }
