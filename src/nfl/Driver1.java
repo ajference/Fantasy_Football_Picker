2878c854d5cd8bce04989eaf8fc7531957dec0af
Alejandro Quintana <aquintana96@email.arizona.edu>
2018-11-04 17:21:31 -0700

    Driver1 Instance 2

--- src/nfl/Driver1.java
+++ src/nfl/Driver1.java
@@ -1,4 +1,5 @@
 package nfl;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
@@ -11,7 +12,9 @@ public class Driver1 {
 	int g = 0;
 	int back = 0;
 	int weekchooser = 0;
+	int firstTime = 0;
 	Scanner scan = new Scanner (System.in);
+	String[] Current = new String[8];
 	while(back == 0) {
 		back = 1;
 		while(g == 0) {
@@ -43,7 +46,7 @@ public class Driver1 {
 				System.out.println("These Are the teams that are playing this week");
 				System.out.println("1 Eagles vs Seahawks \n2 Dolphins vs Patriots\n3 Mo vs Bamba\n4 Justin vs Josh");
 				//NFL.printWeeklySchedule();
-				System.out.println("Choose Which Match you Want to View or hit 9 to go back");
+				System.out.println("Choose Which Match you Want to View or 9 to go back");
 				weekchooser = scan.nextInt();
 				if(weekchooser == 9) {
 					g = 0; 
@@ -56,7 +59,7 @@ public class Driver1 {
 					System.out.println("Team's 1 Ranks is 3 Team 2's Rank is 1 ");
 					System.out.println("The Team favored to win is team 2 ");
 					System.out.println("Key Playres to look out for \nMarek Perez \nAJ Ference \nJoshua Silverio\n");
-					System.out.println("Enter 1 to go back to weeklySchdule or 2 to go back to main screen");
+					System.out.println("Enter 1 to go back to weekly schedule or 2 to go back to main screen");
 					weekchooser = scan.nextInt();
 					if(weekchooser == 2) {
 						g = 0; 
@@ -72,15 +75,66 @@ public class Driver1 {
 			}
 		}
 		if(g == 2) {	//PickGUru
-			System.out.println("Welcom to Pick Guru\nTime To Make a Team!\nLet's Start with The QuarterBack ");
+			String[] Temp = {"JO", "Momma", "is", "so", "nice"};
+			String[] guru = new String[8];
 			
+			while(back == 1) {
+				if(firstTime == 1) {
+					System.out.println("Your Current team is " + Current[0] + " " + Current[1]);
+					System.out.println("Do you want to select a new team? 1 for yes of 2 to go back to main screen");
+					weekchooser = scan.nextInt();
+					if(weekchooser == 1) {
+						firstTime = 0;
+					}
+					if(weekchooser == 2) {
+						g = 0; 
+						back = 0;
+						destination = scan.nextLine();
+					}
+					
+				}
+				if(firstTime == 0) {
+					System.out.println("Welcome to Pick Guru\nTime To Make a Team!\nLet's Start with The QuarterBack\nChoose 1 of the top 5 quarterbacks\n1 JO\n2 Momma\n3 is\n4 so\n5 nice\n");
+					weekchooser = scan.nextInt();
+					guru[0] = Temp[weekchooser -1];
+					System.out.println("Now time to Pick a Running Back Pick, Choose 1 of these 5 Runnings backs\n1 JO\n2 Momma\n3 is\n4 so\n5 nice\n ");
+					weekchooser = scan.nextInt();
+					guru[1] = Temp[weekchooser -1];
+					System.out.println("The team you selected is " + guru[0] + " " + guru[1]);
+					firstTime = 1;
+					Current = guru;
+					
+					System.out.println("Enter 1 to go back to main screen");
+					weekchooser = scan.nextInt();
+					if(weekchooser == 1) {
+						g = 0; 
+						back = 0;
+						destination = scan.nextLine();
+					}
+					}
+				
+			}
 		}
 		if(g == 3) {	//Team Maker
-			System.out.println("Lets make a Team");
+			System.out.println("Lets make a Team\n");
+			System.out.println("Enter 1 to go back to main screen");
+			weekchooser = scan.nextInt();
+			if(weekchooser == 1) {
+				g = 0; 
+				back = 0;
+				destination = scan.nextLine();
+			}
 			
 		}
 		if(g == 4) {	//Top Players
 			System.out.println("These are this weeks top players!");
+			System.out.println("Enter 1 to go back to main screen");
+			weekchooser = scan.nextInt();
+			if(weekchooser == 1) {
+				g = 0; 
+				back = 0;
+				destination = scan.nextLine();
+			}
 			
 		}
 		
