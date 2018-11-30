65907f326255c22dbb8ab4f758e72d1ef5ebfd35
unknown <marekpere98@gmail.com>
2018-11-29 20:02:47 -0700

    error 2

---
+++ src/picker_gui/FantasyGui.java
@@ -0,0 +1,15 @@
+package picker_gui;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
+
+public class FantasyGui {
+
+	
+	
+}
